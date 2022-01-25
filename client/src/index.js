9512c704e178b718a825f6e752420860139d1721
Sangaja Patel <56154737+sangajapatel06@users.noreply.github.com>
2022-01-26 03:25:59 +0530

    actions and reducers

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 
-import App from './App'
+import reducers from './reducers';
+
+import App from './App';
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
+
+
 
 
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
