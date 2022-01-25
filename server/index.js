a866ae33813360ce7e0b98a734a2d57f83c4802a
Sangaja Patel <56154737+sangajapatel06@users.noreply.github.com>
2022-01-26 03:27:42 +0530

    server/index

--- server/index.js
+++ server/index.js
@@ -6,12 +6,13 @@ import cors from 'cors'
 import postRoutes from './routes/posts.js';
 
 const app = express()
+app.use(cors())
+
 
 app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors())
 
 const CONNECTION_URL =
   'mongodb+srv://sangaja:reactmern@cluster0.slnpm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
