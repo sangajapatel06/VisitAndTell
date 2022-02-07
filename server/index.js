c6e0c8d1149aca18f80a7b07c30a5dbf4e22fbfa
Sangaja Patel <56154737+sangajapatel06@users.noreply.github.com>
2022-02-07 23:43:27 +0530

    env setup

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import dotenv from 'dotenv'
 
 import postRoutes from './routes/posts.js';
 
-const app = express()
+const app = express();
+dotenv.config();
 app.use(cors())
 
 
@@ -14,12 +16,12 @@ app.use('/posts', postRoutes);
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 
-const CONNECTION_URL =
-  'mongodb+srv://sangaja:reactmern@cluster0.slnpm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+// const CONNECTION_URL =
+//   'mongodb+srv://sangaja:reactmern@cluster0.slnpm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server Running on Port: http://localhost:${PORT}`)
