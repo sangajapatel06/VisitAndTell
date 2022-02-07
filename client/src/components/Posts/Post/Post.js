33501a3d353f1628792318c6d1ea19c6b6dffa63
Sangaja Patel <56154737+sangajapatel06@users.noreply.github.com>
2022-02-07 21:01:52 +0530

    update functionality (error in card)

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,14 +1,82 @@
+// import React from 'react'
+// import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core/';
+// import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+// import useStyles from './styles'
+// import DeleteIcon from '@material-ui/icons/Delete'
+// import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
+// import ShareIcon from '@material-ui/icons/Share'
+// import moment from 'moment' 
+
+// const Post = ({ post, setCurrentId }) => {
+//     const classes = useStyles()
+//   return (
+//     <Card className={classes.card}>
+//       <CardMedia
+//         className={classes.media}
+//         image={post.selectedFile}
+//         title={post.title}
+//       />
+//       <div className={classes.overlay}>
+//         <Typography variant='h6'>{post.creator}</Typography>
+//         <Typography variant='body2'>
+//           {moment(post.createdAt).fromNow()}
+//         </Typography>
+//       </div>
+//       <div className={classes.overlay2}>
+//         <Button
+//           style={{ color: 'white' }}
+//           size='small'
+//           onClick={() => setCurrentId(post._id)}
+//         >
+//           <MoreHorizIcon fontSize='default' />
+//         </Button>
+//       </div>
+//       <div>
+//         <Typography variant='body2' color='textSecondary'>
+//           {post.tags.map((tag) => `#${tag} `)}
+//         </Typography>
+//       </div>
+//       <Typography className={classes.title} variant='h5' gutterBottom>
+//         {post.title}
+//       </Typography>
+//       <CardContent>
+//         <Typography  variant='h5' gutterBottom>
+//           {post.title}
+//         </Typography>
+//       </CardContent>
+//       <CardActions className={classes.cardActions}>
+//         <Button size='small' color='primary' onClick={() => {}}>
+//           <ThumbUpAltIcon fontSize='small' />
+//           Like
+//           {post.likeCount}
+//         </Button>
+//         <Button size='small' color='primary' onClick={() => {}}>
+//           <DeleteIcon fontSize='small' />
+//           Delete
+//         </Button>
+//       </CardActions>
+//     </Card>
+//   )
+// }
+
+// export default Post
+
 import React from 'react'
-import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core/';
-import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
-import useStyles from './styles'
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from '@material-ui/core/'
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt'
 import DeleteIcon from '@material-ui/icons/Delete'
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
-import ShareIcon from '@material-ui/icons/Share'
-import moment from 'moment' 
-
-const Post = ({ post }) => {
-    const classes = useStyles()
+import moment from 'moment'
+import useStyles from './styles'
+const Post = ({ post, setCurrentId }) => {
+  const classes = useStyles()
   return (
     <Card className={classes.card}>
       <CardMedia
@@ -23,28 +91,42 @@ const Post = ({ post }) => {
         </Typography>
       </div>
       <div className={classes.overlay2}>
-        <Button style={{ color: 'white' }} size='small' onClick={() => {}}>
+        <Button
+          style={{ color: 'white' }}
+          size='small'
+          onClick={() => setCurrentId(post._id)}
+        >
           <MoreHorizIcon fontSize='default' />
         </Button>
       </div>
-      <div>
-        <Typography variant='body2' color='textSecondary'>
+      <div className={classes.details}>
+        <Typography variant='body2' color='textSecondary' component='h2'>
           {post.tags.map((tag) => `#${tag} `)}
         </Typography>
       </div>
+      <Typography
+        className={classes.title}
+        gutterBottom
+        variant='h5'
+        component='h2'
+      >
+        {post.title}
+      </Typography>
       <CardContent>
-        <Typography className={classes.title} variant='h5' gutterBottom>
+        <Typography variant='body2' color='textSecondary' component='p'>
           {post.message}
         </Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
-        <Button size="small" color="primary" onClick={() => {}}>
-          <ThumbUpAltIcon fontSize="small"/>
+        <Button size='small' color='primary' onClick={() => {}}>
+          <ThumbUpAltIcon fontSize='small' />
           Like
           {post.likeCount}
         </Button>
-        <Button size="small" color="primary" onClick={() => {}}>
-          <DeleteIcon fontSize="small"/>
+      </CardActions>
+      <CardActions className={classes.cardActions}>
+        <Button size='small' color='primary' onClick={() => {}}>
+          <DeleteIcon fontSize='small' />
           Delete
         </Button>
       </CardActions>
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,28 +1,45 @@
 // handlers for routes
-import PostMessage from "../models/postMessage.js";
+// import { mongoose } from "mongoose";
+import pkg from 'mongoose'
+const { mongoose } = pkg
+import PostMessage from '../models/postMessage.js'
 
 export const getPosts = async (req, res) => {
-    try {
-        const postMessages = await PostMessage.find();
-        // console.log(postMessage);
+  try {
+    const postMessages = await PostMessage.find()
+    // console.log(postMessage);
 
-        res.status(200).json(postMessages);
-
-    } catch (error){
-        res.status(404).json({ message: error.message });
-    }
+    res.status(200).json(postMessages)
+  } catch (error) {
+    res.status(404).json({ message: error.message })
+  }
 }
 
-export const createPost = async(req,res) => {
-    const post = req.body;
+export const createPost = async (req, res) => {
+  const post = req.body
 
-    const newPost = new PostMessage(post);
-    try {
-        newPost.save();
+  const newPost = new PostMessage(post)
+  try {
+    newPost.save()
 
-        res.status(201).json(newPost);
-    } catch (error) {
-        res.status(409).json({message: error.message });
-        
-    }
+    res.status(201).json(newPost)
+  } catch (error) {
+    res.status(409).json({ message: error.message })
+  }
+}
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = res.params
+  const post = req.body
+
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send('No post with that id')
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    _id,
+    { ...post, _id },
+    { new: true }
+  )
+
+  res.json(updatedPost)
 }
